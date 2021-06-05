bab75bb7206b99bbf47b6ea9eb33288a3a903e20
Adam1107 <72392986+Adam11072000@users.noreply.github.com>
2021-06-05 19:55:27 +0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,15 @@
+echo running symbol table tests...
+../binutils-2.36.1/binutils/readelf -s ../../prog > output/prog_symbolTable_out.txt
+../binutils-2.36.1/binutils/readelf -s ../../part2/a.o > output/a_symbolTable_out.txt
+../binutils-2.36.1/binutils/readelf -s ../../part2/b.o > output/b_symbolTable_out.txt
+diff -q expected/a_symbolTable_expected.txt output/a_symbolTable_out.txt
+diff -q expected/b_symbolTable_expected.txt output/b_symbolTable_out.txt
+diff -q expected/prog_symbolTable_expected.txt output/prog_symbolTable_out.txt
+echo running section table tests...
+../binutils-2.36.1/binutils/readelf -S ../../prog > output/prog_sectionTable_out.txt
+../binutils-2.36.1/binutils/readelf -S ../../part2/a.o > output/a_sectionTable_out.txt
+../binutils-2.36.1/binutils/readelf -S ../../part2/b.o > output/b_sectionTable_out.txt
+diff -q expected/a_sectionTable_expected.txt output/a_sectionTable_out.txt
+diff -q expected/b_sectionTable_expected.txt output/b_sectionTable_out.txt
+diff -q expected/prog_sectionTable_expected.txt output/prog_sectionTable_out.txt
+echo done!
