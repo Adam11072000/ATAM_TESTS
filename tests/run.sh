278f17315b29f08e117086272d4e18efea0a616b
Adam1107 <72392986+Adam11072000@users.noreply.github.com>
2021-06-07 07:30:10 +0300

    Add files via upload

--- tests/run.sh
+++ tests/run.sh
@@ -11,6 +11,7 @@ diff -q expected/c_symbolTable_expected.txt output/c_symbolTable_out.txt
 diff -q expected/d_symbolTable_expected.txt output/d_symbolTable_out.txt
 echo running section table tests...
 ../part1/binutils-2.36.1/binutils/readelf -S ../e.o > output/e_sectionTable_out.txt
+../part1/binutils-2.36.1/binutils/readelf -S ../prog > output/prog_sectionTable_out.txt
 ../part1/binutils-2.36.1/binutils/readelf -S ../part2/a.o > output/a_sectionTable_out.txt
 ../part1/binutils-2.36.1/binutils/readelf -S ../part2/b.o > output/b_sectionTable_out.txt
 ../part1/binutils-2.36.1/binutils/readelf -S ../c.o > output/c_sectionTable_out.txt
@@ -20,4 +21,5 @@ diff -q expected/b_sectionTable_expected.txt output/b_sectionTable_out.txt
 diff -q expected/e_sectionTable_expected.txt output/e_sectionTable_out.txt
 diff -q expected/c_sectionTable_expected.txt output/c_sectionTable_out.txt
 diff -q expected/d_sectionTable_expected.txt output/d_sectionTable_out.txt
+diff -q expected/prog_sectionTable_expected.txt output/prog_sectionTable_out.txt
 echo done!
